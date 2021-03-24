9adf66b4ea931f45014cfc6f23c3b15dbedc2b9d
Anton Fadeev <weutnyc@gmail.com>
2021-03-24 18:48:38 +0300

    button constrains

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var strartButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        strartButton.layer.cornerRadius = 10
+        yellowView.alpha = 0.3
+        redView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        yellowView.layer.cornerRadius = 60
+        redView.layer.cornerRadius = 60
+        greenView.layer.cornerRadius = 60
     }
 
-
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
